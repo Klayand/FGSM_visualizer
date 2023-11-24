fa657e1c833b20527884c0c8cd8ebf25598cfbf2
Klayand <996899477@qq.com>
2023-11-24 11:05:51 +0800

    beautify the readme.md

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 
 def run_streamlit():
-    py_process = subprocess.call(['streamlit', 'run',  os.path.abspath(os.path.join(os.getcwd(), 'app.py.py'))])
+    py_process = subprocess.call(['streamlit', 'run',  os.path.abspath(os.path.join(os.getcwd(), 'app.py'))])
 
 run_streamlit()
 
